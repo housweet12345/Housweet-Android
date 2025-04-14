25290d43ee6d994b018f122051b7d8a423ef203d
hoseong <hoseong091234@gmail.com>
2025-04-14 22:30:24 +0900

    프로필 화면 임시 구현

---
+++ presentation/src/main/java/com/housweet/presentation/ui/theme/ColorGroup.kt
@@ -0,0 +1,9 @@
+package com.housweet.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ColorGroup {
+    val Primary = Color(0xFF665ED3)
+    val Black_1E1E1E = Color(0xFF1E1E1E)
+    val Black_313131 = Color(0xFF313131)
+}
