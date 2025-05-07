232b48d7cdc26cebe07613d684c61f374aac6129
전하윤 <kjklove0701@gmail.com>
2025-05-07 20:05:21 +0900

    feat: 하우스 등록 2단계(추가 정보 입력) 화면 기본 UI 구현

---
+++ presentation/src/main/java/com/housweet/presentation/navigation/HouseRegisterNav.kt
@@ -0,0 +1,4 @@
+package com.housweet.presentation.navigation
+
+class HouseRegisterNav {
+}
---
+++ presentation/src/main/java/com/housweet/presentation/ui/chatlist/DummyChatList.kt
@@ -0,0 +1,5 @@
+package com.housweet.presentation.ui.chatlist
+
+//UI 개발용 더미 데이터
+class DummyChatList {
+}
