0d2c2d3f15d677eedf428ab8517527df50ccb15b
전하윤 <kjklove0701@gmail.com>
2025-06-07 21:13:40 +0900

    Initial commit: Upload 방구하기 project

--- presentation/src/main/java/com/housweet/presentation/ui/common/StepIndicator.kt
+++ presentation/src/main/java/com/housweet/presentation/ui/common/StepIndicator.kt
@@ -1,22 +1,32 @@
 package com.housweet.presentation.ui.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.housweet.presentation.R
 
 @Composable
 fun StepIndicator(currentStep: Int) {
     val steps = listOf("하우스 특징", "추가 정보 입력", "사진 업로드", "선호하는 사람")
+    val stepImages: List<Pair<Int, Int>> = listOf(
+        Pair(R.drawable.btn_purple_1, R.drawable.btn_gray_1),
+        Pair(R.drawable.btn_purple_2, R.drawable.btn_gray_2),
+        Pair(R.drawable.btn_purple_3, R.drawable.btn_gray_3),
+        Pair(R.drawable.btn_purple_4, R.drawable.btn_gray_4),
+    )
 
     Row(
         modifier = Modifier
@@ -25,16 +35,24 @@ fun StepIndicator(currentStep: Int) {
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         steps.forEachIndexed { index, title ->
+            val isActive = currentStep == index + 1
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = "${index + 1}",
-                    color = if (currentStep == index + 1) Color(0xFF6C4DFF) else Color.Gray,
-                    fontWeight = FontWeight.Bold
+                Image(
+                    painter = painterResource(id = if(isActive) stepImages[index].first else stepImages[index].second),
+                    contentDescription = "Step ${index + 1}",
+                    modifier = Modifier.size(24.dp)
                 )
+//                Text(
+//                    text = "${index + 1}",
+//                    color = if (currentStep == index + 1) Color(0xFF6C4DFF) else Color.Gray,
+//                    fontWeight = FontWeight.Bold
+//                )
                 Text(
                     text = title,
                     fontSize = 12.sp,
-                    color = if (currentStep == index + 1) Color(0xFF6C4DFF) else Color.Gray
+                    color = if (isActive) Color(0xFF6C4DFF) else Color.Gray,
+                    fontWeight = if (isActive) FontWeight.Bold else FontWeight.Normal,
+                    modifier = Modifier.padding(top=4.dp)
                 )
             }
         }
--- presentation/src/main/java/com/housweet/presentation/ui/registerhouse/HouseRegisterScreen3.kt
+++ presentation/src/main/java/com/housweet/presentation/ui/registerhouse/HouseRegisterScreen3.kt
@@ -1,4 +1,123 @@
 package com.housweet.presentation.ui.registerhouse
 
-class HouseRegisterScreen3 {
+import android.graphics.Bitmap
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.housweet.presentation.R
+import com.housweet.presentation.ui.common.StepIndicator
+import com.housweet.presentation.ui.common.TopBarWithBackButton
+
+@Composable
+fun HouseRegisterScreen3 (
+    onNextClick: () -> Unit,
+    onBackClick: () -> Unit,
+    onImagePickClick: () -> Unit,
+    selectedImageBitmap: Bitmap? = null
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 16.dp)
+    ) {
+        TopBarWithBackButton(title = "하우스 올리기", onBackClick = onBackClick)
+
+        StepIndicator(currentStep = 3)
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "실제 사진을 첨부해주세요.",
+                color = Color(0xFF6C4DFF),
+                fontSize = 14.sp,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "사진 첨부",
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .border(1.dp, Color.Gray, shape = RoundedCornerShape(12.dp))
+                .background(Color.White)
+                .clickable { onImagePickClick() },
+            contentAlignment = Alignment.Center
+        ) {
+            if (selectedImageBitmap == null) {
+                Icon(
+                    painter = painterResource(id = R.drawable.camera_icon_gray),
+                    contentDescription = "사진 선택",
+                    tint = Color.Gray,
+                    modifier = Modifier.size(48.dp)
+                )
+            } else {
+                Image(
+                    bitmap = selectedImageBitmap.asImageBitmap(),
+                    contentDescription = "첨부된 이미지",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = onNextClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = RoundedCornerShape(6.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF665ED3),
+                contentColor = Color.White
+            )
+        ) {
+            Text("다음")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+    }
 }
