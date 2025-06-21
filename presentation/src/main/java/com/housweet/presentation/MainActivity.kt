169bde7a53713649e3d998edadccccb2f4e48767
hoseong <hoseong091234@gmail.com>
2025-06-21 20:00:06 +0900

    Home 화면 추가

--- presentation/src/main/java/com/housweet/presentation/MainActivity.kt
+++ presentation/src/main/java/com/housweet/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.housweet.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -10,7 +13,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.housweet.presentation.ui.chat.ChatScreen
 import com.housweet.presentation.ui.chatlist.ChatListScreen
+import com.housweet.presentation.ui.home.HomeScreen
 import com.housweet.presentation.ui.navigation.BottomNavItem
+import com.housweet.presentation.ui.navigation.BottomNavigation
 import com.housweet.presentation.ui.profile.route.EditProfileKeyWordRoute
 import com.housweet.presentation.ui.profile.route.EditProfileRoute
 import com.housweet.presentation.ui.profile.route.MyProfileRoute
@@ -23,96 +28,104 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            NavHost(
-                navController = navController,
-                startDestination = "house_register_1"
+            Scaffold(
+                bottomBar = { BottomNavigation(navController) },
             ) {
-                composable(BottomNavItem.Home.route) {
-                    //홈화면
-                }
-                composable(BottomNavItem.Calendar.route) {
-                    //캘린더 화면
-                }
-                composable(BottomNavItem.FinanceLedger.route) {
-                    //가계부 화면
-                }
-                composable(BottomNavItem.Notice.route) {
-                    //공지사항 화면
-                }
-                composable("house_register_1") {
-                    HouseRegisterScreen1( onNextClick = {
-                        navController.navigate("house_register_2")
-                    },
-                        onBackClick = {
-                            navController.popBackStack()
-                        }
-                    )
-                }
-                composable("house_register_2") {
-                    HouseRegisterScreen2(
-                        onNextClick = {
-                            navController.navigate("house_register_3")
-                        },
-                        onBackClick = {navController.navigate("house_register_1")},
-                        onRegionSelectClick = {
-                            navController.navigate("region_select")
-                        }
-                    )
-                }
-                composable("house_register_3") {
-                    HouseRegisterScreen3(
-                        onNextClick = {
-                            navController.navigate("house_register_4")
-                        },
-                        onBackClick = { navController.navigate("house_register_2") },
-                        onImagePickClick = {},
-                        selectedImageBitmap = null
-                    )
-                }
-                composable("house_register_4") {
-                    HouseRegisterScreen4(
-                        onBackClick = {
-                            navController.navigate("house_register_3")
-                        },
-                        onCompleteClick = {}
-                    )
-                }
-                composable("chat_list") {
-                    ChatListScreen(navController)
-                }
-                composable("chat_detail/{chatName}") { backStackEntry ->
-                    val chatName = backStackEntry.arguments?.getString("chatName") ?: "Unknown"
-                    ChatScreen(chatName, navController)
-                }
-                composable("profile/me") {
-                    MyProfileRoute(
-                        navigateEditProfile = { navController.navigate("profile/edit") },
-                        onBackClick = { navController.popBackStack() },
-                        navigateChatting = {  }
-                    )
-                }
-                composable("profile/edit") {
-                    EditProfileRoute(
-                        onBackClick = { navController.popBackStack() },
-                        navigateEditKeyword = { navController.navigate("profile/edit_keyword") }
-                    )
-                }
-
-                composable("profile/edit_keyword") { navBackStackEntry ->
-                    val parentEntry = remember(navBackStackEntry) {
-                        navController.getBackStackEntry("profile/edit")
+                NavHost(
+                    navController = navController,
+                    startDestination = BottomNavItem.Home.route
+                ) {
+                    composable(BottomNavItem.Home.route) {
+                        HomeScreen()
                     }
-                    val viewModel: EditProfileViewModel = hiltViewModel(parentEntry)
+                    composable(BottomNavItem.Calendar.route) {
+                        //캘린더 화면
+                        Text("캘린더")
+                    }
+                    composable(BottomNavItem.FinanceLedger.route) {
+                        //가계부 화면
+                        Text("가계부")
+                    }
+                    composable(BottomNavItem.Notice.route) {
+                        //공지사항 화면
+                        Text("공지사항")
+                    }
+                    composable("house_register_1") {
+                        HouseRegisterScreen1( onNextClick = {
+                            navController.navigate("house_register_2")
+                        },
+                            onBackClick = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                    composable("house_register_2") {
+                        HouseRegisterScreen2(
+                            onNextClick = {
+                                navController.navigate("house_register_3")
+                            },
+                            onBackClick = {navController.navigate("house_register_1")},
+                            onRegionSelectClick = {
+                                navController.navigate("region_select")
+                            }
+                        )
+                    }
+                    composable("house_register_3") {
+                        HouseRegisterScreen3(
+                            onNextClick = {
+                                navController.navigate("house_register_4")
+                            },
+                            onBackClick = { navController.navigate("house_register_2") },
+                            onImagePickClick = {},
+                            selectedImageBitmap = null
+                        )
+                    }
+                    composable("house_register_4") {
+                        HouseRegisterScreen4(
+                            onBackClick = {
+                                navController.navigate("house_register_3")
+                            },
+                            onCompleteClick = {}
+                        )
+                    }
+                    composable("chat_list") {
+                        ChatListScreen(navController)
+                    }
+                    composable("chat_detail/{chatName}") { backStackEntry ->
+                        val chatName = backStackEntry.arguments?.getString("chatName") ?: "Unknown"
+                        ChatScreen(chatName, navController)
+                    }
+                    composable("profile/me") {
+                        MyProfileRoute(
+                            navigateEditProfile = { navController.navigate("profile/edit") },
+                            onBackClick = { navController.popBackStack() },
+                            navigateChatting = {  }
+                        )
+                    }
+                    composable("profile/edit") {
+                        EditProfileRoute(
+                            onBackClick = { navController.popBackStack() },
+                            navigateEditKeyword = { navController.navigate("profile/edit_keyword") }
+                        )
+                    }
 
-                    EditProfileKeyWordRoute(
-                        viewModel = viewModel,
-                        onBackClick = { navController.popBackStack() },
-                        navigateMyProfile = { navController.navigate("profile/me") }
-                    )
+                    composable("profile/edit_keyword") { navBackStackEntry ->
+                        val parentEntry = remember(navBackStackEntry) {
+                            navController.getBackStackEntry("profile/edit")
+                        }
+                        val viewModel: EditProfileViewModel = hiltViewModel(parentEntry)
+
+                        EditProfileKeyWordRoute(
+                            viewModel = viewModel,
+                            onBackClick = { navController.popBackStack() },
+                            navigateMyProfile = { navController.navigate("profile/me") }
+                        )
+                    }
                 }
             }
         }
