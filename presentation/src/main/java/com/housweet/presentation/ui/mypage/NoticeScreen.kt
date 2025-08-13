6c498702dcae89338f51d65e2012a6342606de32
전하윤 <kjklove0701@gmail.com>
2025-08-14 04:00:58 +0900

    fix : 하우스 올리기 Alert 추가
    태그 선택 및 입력창에 미입력 시 Alert 기능 추가함.

--- presentation/src/main/java/com/housweet/presentation/ui/mypage/NoticeScreen.kt
+++ presentation/src/main/java/com/housweet/presentation/ui/mypage/NoticeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
