903a7bed4119ae16841f9bf80d45a49752969d50
Hoseong-Ryu <hoseong091234@gmail.com>
2025-08-16 22:46:24 +0900

    feat: 프로필 수정 후 백스택 처리

--- presentation/src/main/java/com/housweet/presentation/ui/profile/route/EditProfileRoute.kt
+++ presentation/src/main/java/com/housweet/presentation/ui/profile/route/EditProfileRoute.kt
@@ -1,6 +1,9 @@
 package com.housweet.presentation.ui.profile.route
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.housweet.presentation.ui.profile.screen.EditProfileScreen
@@ -11,9 +14,26 @@ import com.housweet.presentation.viewmodel.profile.EditProfileViewModel
 fun EditProfileRoute(
     viewModel: EditProfileViewModel = hiltViewModel(),
     onBackClick: () -> Unit = {},
+    onSuccessNavigateBack: () -> Unit = {},
     navigateEditKeyword: () -> Unit = {},
 ) {
     val state = viewModel.profileState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    LaunchedEffect(state.value) {
+        when (state.value) {
+            is ProfileInfoState.Error -> {
+                val errorMessage = (state.value as ProfileInfoState.Error).message
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+                onBackClick()
+            }
+            is ProfileInfoState.EditSuccess -> {
+                Toast.makeText(context, "프로필이 성공적으로 수정되었습니다", Toast.LENGTH_SHORT).show()
+                onSuccessNavigateBack()
+            }
+            else -> {}
+        }
+    }
 
     when(state.value) {
         is ProfileInfoState.Success -> {
