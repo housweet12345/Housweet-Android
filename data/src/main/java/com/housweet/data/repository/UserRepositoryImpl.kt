65c2a20085a1c31c1d8aaf3cd72a119e8b4cc7bf
Hoseong-Ryu <hoseong091234@gmail.com>
2025-08-16 21:21:01 +0900

    feat: 프로필 수정 api mapping

--- data/src/main/java/com/housweet/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/housweet/data/repository/UserRepositoryImpl.kt
@@ -37,7 +37,7 @@ class UserRepositoryImpl @Inject constructor(
     override suspend fun updateProfile(userId: String, updatedProfile: ProfileUpdateModel): Result<ProfileUpdateModel> {
         return runCatching {
             val patchDto = updatedProfile.toProfilePatchDto()
-            val response: ProfileUpdateDto = client.patch("${BuildConfig.USER_BASE_URL}/profile/$userId/update") {
+            val response: ProfileUpdateDto = client.patch("${BuildConfig.USER_BASE_URL}/profile/$userId/update/") {
                 setBody(patchDto)
             }.body()
             response.mapToProfileUpdateModel()
