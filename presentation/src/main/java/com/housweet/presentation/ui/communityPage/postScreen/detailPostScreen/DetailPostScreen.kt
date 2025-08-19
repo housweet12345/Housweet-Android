027049e74ba3c2dfaf768e3ae96913b6608718ac
hoijun <65825603+hoijun@users.noreply.github.com>
2025-08-19 17:14:03 +0900

    feat: 방 공고 페이지 태그 분류 수정.

--- presentation/src/main/java/com/housweet/presentation/ui/communityPage/postScreen/detailPostScreen/DetailPostScreen.kt
+++ presentation/src/main/java/com/housweet/presentation/ui/communityPage/postScreen/detailPostScreen/DetailPostScreen.kt
@@ -483,7 +483,7 @@ private fun HouseFeatures(
 
     FeaturesLayout(
         maxLines = 4,
-        featureList = roomPostDetail.trafficTags + roomPostDetail.sizeOfHouseTags + roomPostDetail.infraTags + roomPostDetail.personalityTags
+        featureList = roomPostDetail.trafficTags + roomPostDetail.sizeOfHouseTags + roomPostDetail.infraTags
     )
 }
 
@@ -504,7 +504,7 @@ private fun PreferredFeatures(
 
     FeaturesLayout(
         maxLines = 3,
-        featureList = roomPostDetail.lifePatternTags + roomPostDetail.tidyingUpHabitTags
+        featureList = roomPostDetail.personalityTags + roomPostDetail.lifePatternTags + roomPostDetail.tidyingUpHabitTags
     )
 }
 
