ffa9f8cfa249657b2068d3780a6a7c6adec2e9d7
전하윤 <kjklove0701@gmail.com>
2025-08-22 02:26:52 +0900

    fix: 앱스토어에서 앱 업데이트 불가 -> 플레이콘솔과 build.gradle의 versionCode와 versionName이 일치하지 않을 경우에 발생할 수 있다고 하여 맞춰서 수정함.
    구글 플레이스토어에서 기기에서 앱 업데이트가 안되는 문제 발생.
    버전이 build.gradle에서 업데이트가 안됐을수도 있다고 하여
    확인해보니 code = 1, name = 1.0 으로 변경 안된 것을 확인함.
    처음에는 임시로 build.gradle code = 2, name = 2.0 으로 변경해 커밋했으나,
    혹시 몰라서 플레이콘솔에서 확인해보니,
    versionCode = 2, versionName = 1.1 로 설정되어 있어서
    build.gradle에서 동일하게 업데이트 함.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 2
-        versionName = "2.0"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
