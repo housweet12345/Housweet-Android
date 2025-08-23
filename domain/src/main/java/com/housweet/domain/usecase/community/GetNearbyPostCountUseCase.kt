3d42ac82f8bedc0e39ad8d9648450e88d6917ac3
Hoseong-Ryu <hoseong091234@gmail.com>
2025-08-23 15:20:00 +0900

    refactor: 개행 정리

--- domain/src/main/java/com/housweet/domain/usecase/community/GetNearbyPostCountUseCase.kt
+++ domain/src/main/java/com/housweet/domain/usecase/community/GetNearbyPostCountUseCase.kt
@@ -3,4 +3,5 @@ package com.housweet.domain.usecase.community
 import com.housweet.domain.model.NearByPostCountDataModel
 import kotlinx.coroutines.flow.Flow
 
-fun interface GetNearbyPostCountUseCase : suspend (Double, Double, Int) -> Flow<Result<List<NearByPostCountDataModel>>>
+fun interface GetNearbyPostCountUseCase :
+    suspend (Double, Double, Int) -> Flow<Result<List<NearByPostCountDataModel>>>
--- domain/src/main/java/com/housweet/domain/usecase/community/GetRoomPostsByLocationUseCase.kt
+++ domain/src/main/java/com/housweet/domain/usecase/community/GetRoomPostsByLocationUseCase.kt
@@ -3,4 +3,5 @@ package com.housweet.domain.usecase.community
 import com.housweet.domain.model.RoomPostsByLocationDataModel
 import kotlinx.coroutines.flow.Flow
 
-fun interface GetRoomPostsByLocationUseCase : suspend (String) -> Flow<Result<List<RoomPostsByLocationDataModel>>>
+fun interface GetRoomPostsByLocationUseCase :
+    suspend (String) -> Flow<Result<List<RoomPostsByLocationDataModel>>>
