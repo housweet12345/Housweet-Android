5178be0512f06fb5557b57cca306f1da9a9ddd95
전하윤 <kjklove0701@gmail.com>
2025-09-06 15:14:46 +0900

    chore: versionCode, versionName 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.housweet.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
